9f7282b0e796469cd6e6e9b228c6ab882036a05b
Khizar Wakeel <khizarwakeel@gmail.com>
2024-03-05 19:30:40 +0500

    Initial commit

---
+++ 1. Assignment/App.js
@@ -0,0 +1,123 @@
+// Question 1
+
+// alert("Welcome to my beautiful Website");
+
+// Question 2
+
+// alert("Error! Please Enter a valid password");
+
+// Question 3
+
+// alert("Welcome to JS Land... \n Happy Coding!")
+
+// Question 4
+
+// alert("Welcome to JS Land...");
+
+// alert("Happy Coding \n <input type='checkbox' /> Prevent this page from additional dialogs");
+
+// Question 5
+
+// console.log("Hello... I can run JS through my web browser's console");
+
+// Question 6
+// Question 7
+
+// Question 8
+
+// var userName;
+
+// Question 9
+
+// var myName = "Khizar Wakeel";
+
+// Question 10
+
+// var message;
+
+// message = "Hello World";
+
+// console.log(message);
+
+// Question 11
+
+// var stdName = "Khizar Wakeel";
+
+// alert(stdName);
+
+// var stdAge = 24;
+
+// alert(stdAge);
+
+// var stdCourse = "Certified Web and Mobile Application Development";
+
+// alert(stdCourse);
+
+// Question 12
+
+// var order = "Pizza";
+
+// console.log(order);
+// console.log(order.slice(0, 4));
+// console.log(order.slice(0, 3));
+// console.log(order.slice(0, 2));
+// console.log(order.slice(0, 1));
+
+// Question 13
+
+// var email = "khizarwakeel@gmail.com";
+
+// alert("My email address is "+email)
+
+// Question 14
+
+// var book = "“A smarter way to learn JavaScript”";
+// alert("I am trying to learn from the book " + book);
+
+// Question 15
+
+// var userMessage = "Yah! I can write HTML content through JavaScript";
+
+// document.write(userMessage);
+
+// Question 16
+
+// var design = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+
+// alert(design);
+
+// document.write(design);
+
+// Question 17
+
+// var age;
+
+// age = 24;
+
+// alert("I am " + age + " years old!");
+
+// Question 18
+
+// Question 2 must Complete
+
+// Question 19
+
+// var birthYear;
+
+// birthYear = 1999;
+
+// document.write("I was born in " + birthYear);
+// document.write(
+//   "<br />Data type of my declared variable is " + typeof birthYear
+// );
+
+// Question 20
+
+// var vistorName = "John Doe";
+// var productTitle = "IPhone";
+// var quantity = 5;
+// var onlineShop = "www.amazon.com";
+
+// document.write(
+//   vistorName + " Ordered " + quantity + " T-Shirt(s) on " + onlineShop
+// );
