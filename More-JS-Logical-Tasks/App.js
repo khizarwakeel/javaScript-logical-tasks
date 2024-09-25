82b70cc23351737f79a417d37801897b122b6159
Khizar Wakeel <khizarwakeel@gmail.com>
2024-09-26 02:30:28 +0500

    add anagram task

--- More-JS-Logical-Tasks/App.js
+++ More-JS-Logical-Tasks/App.js
@@ -1,1 +1,22 @@
-console.log("Testing");
+// Find Anagram Words
+
+function anagramsFunc(arrayOfWords) {
+    let anagrams = {};
+    for (let word of arrayOfWords) {
+        let sortedWords = word.split('').sort().join('');
+
+        if (!anagrams[sortedWords]) {
+            anagrams[sortedWords] = [];
+        }
+
+        anagrams[sortedWords].push(word);
+    }
+
+    console.log(anagrams);
+    return Object.values(anagrams);
+}
+
+let arrayOfWords = ["bat", "tap", "cat", "tab", "fats", "pat", "khizar", "fast"];
+let newAnagramArray = anagramsFunc(arrayOfWords);
+
+console.log(newAnagramArray);
