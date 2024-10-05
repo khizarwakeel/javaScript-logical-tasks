b338d0dacaa4c2e15f518698718ec1026c9ba915
Khizar Wakeel <khizarwakeel@gmail.com>
2024-10-05 23:29:09 +0500

    practice question 5 lydia

--- lydia-js-questions/App.js
+++ lydia-js-questions/App.js
@@ -56,3 +56,19 @@ console.log(!'Lydia');
 // A: 1 and false
 // B: false and NaN
 // C: false and false
+
+// Question 5
+
+const bird = {
+    size: 'small',
+};
+
+const mouse = {
+    name: 'Mickey',
+    small: true,
+};
+
+// A: mouse.bird.size is not valid
+// B: mouse[bird.size] is not valid
+// C: mouse[bird["size"]] is not valid
+// D: All of them are valid
