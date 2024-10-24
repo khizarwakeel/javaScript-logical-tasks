1bd2e8c20cba9030f28e54f59c671bca316f9c4b
Khizar Wakeel <khizarwakeel@gmail.com>
2024-10-24 22:51:58 +0500

    add FizzBuzz Logic

--- More-JS-Logical-Tasks/App.js
+++ More-JS-Logical-Tasks/App.js
@@ -278,3 +278,21 @@ function fibonacci(n) {
     return sequence;
 }
 console.log(fibonacci(6));
+
+// FizzBuzz
+
+function fizzBuzz() {
+    for (let i = 1; i <= 100; i++) {
+        if (i % 3 === 0 && i % 5 === 0) {
+            console.log("FizzBuzz");
+        } else if (i % 3 === 0) {
+            console.log("Fizz");
+        } else if (i % 5 === 0) {
+            console.log("Buzz");
+        } else {
+            console.log(i);
+        }
+    }
+}
+
+fizzBuzz();
